73339fb4e088100d138aaf981405f6b53bf046ce
Christian Pezzin <christian.pezzin@gmail.com>
2025-10-29 15:04:20 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 
 export const metadata = {
   title: 'Nextor University',
-  description: 'Framework per gestire landing page dinamiche in base alla richiesta dell'utente.',
+  description: 'Framework per gestire landing page dinamiche in base alle richieste degli utenti.',
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
